bd1243a6b22cf2cbcb35c47e797cc6407009184c
Marisol-li <marisosalazar9@gmail.com>
2024-10-25 19:23:34 -0600

    commit 5
    Actividad_01 Encuesta de Programación
    
    
    Realizar un programa usando VUE, que permita al usuario generar una encuesta de programación de acuerdo a la siguiente imagen.

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            respuesta: '',
+            mostrar: false,
+            donde: '',
+            visitados: '',
+            proximo: '',
+            verResu: false
+        };
+    },
+    methods: {
+        mostrarPreguntas() {
+            this.mostrar = this.respuesta === 'si';
+            this.verResu = false;
+        },
+        mostrarResultados() {
+            this.verResu = true;
+        }
+    }
+});
+
+app.mount('#app'); 
